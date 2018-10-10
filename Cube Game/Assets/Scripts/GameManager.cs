6d4183c402ab1694fb20fe15ff0ffca077dd0064
sohamdesh <sohamdes@buffalo.edu>
2018-10-10 13:36:53 -0400

    Cube game uploaded
    Take your cube to it's destination, avoiding the enemy red cubes.

---
+++ Cube Game/Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager: MonoBehaviour {
+
+    public static int currentScore;
+    public static int highScore;
+
+    public static int currentLevel =0;
+    public static int unlockedLevel;
+
+    // Use this for initialization
+    public static void CompleteLevel() {
+
+        if(currentLevel < 2){
+            currentLevel += 1;
+            SceneManager.LoadScene(currentLevel);
+        }
+        else{
+            print("You Win!");
+        }
+    }
+}
---
+++ Cube Game/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerMovement : MonoBehaviour {
+    public float moveSpeed;
+    public GameObject deathParticles;
+    private float maxSpeed = 5;
+
+    private Vector3 input;
+    private Vector3 spawn;
+    
+	// Use this for initialization
+	void Start () {
+        spawn = transform.position;
+
+	}
+	
+
+	void Update () {
+        //       input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+
+        //       if (GetComponent<Rigidbody>().velocity < maxSpeed){
+
+        //           GetComponent<Rigidbody>().AddForce(input * moveSpeed);
+
+
+        //       }
+
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(horizontal,0, vertical);
+        transform.position += movement * Time.deltaTime * maxSpeed;
+
+        if(transform.position.y < -2 ){
+            Die();
+        }
+
+    }
+
+     void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Enemy")){
+            Die();
+        }
+
+        if(collision.gameObject.CompareTag("Goal"))
+        {
+            GameManager.CompleteLevel();
+        }
+    }
+
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    if(other.transform.tag == "Goal"){
+    //        GameManager.CompleteLevel();
+    //    }
+    //}
+
+    //void OnCollisionEnter(Collision collision)
+    //{
+    //    if (collision.gameObject.CompareTag("Goal"))
+    //    {
+    //        GameManager.CompleteLevel();
+    //    }
+    //}
+
+    void Die(){
+
+        Instantiate(deathParticles, transform.position, Quaternion.Euler(270, 0, 0));
+        transform.position = spawn;
+    }
+}
